cbe48403069c9e2ace7a6c4f20f3db7f24c36d0f
Prince <prince.anim@meltwater.org>
2015-03-17 13:11:39 +0000

    Pre-launch covert page

---
+++ both/router.js
@@ -0,0 +1,26 @@
+/*
+router style, initial configuration template
+layoutTemplate will map to a layout template
+notFoundTemplate will use a template named 404 or a name of your choice if you chose to change it
+loadingTemplate will use a template with loading or a template
+
+*/
+Router.configure({
+	layoutTemplate: 'layout'
+	// notFoundTemplate: '404',
+	// loadingTemplate: 'loading'
+});
+
+// ============================================== routes ========================================================================
+
+//  route one basic
+Router.route ('/', function(){
+	this.render('home');
+	name: 'home'
+});
+
+
+Router.route ('/success', function(){
+	this.render('success');
+	name: 'success'
+});

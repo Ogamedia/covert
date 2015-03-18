0c2791f525e8e479478b823b4de0ada0efbe34b6
Ogamedia <kwame.yeboah@meltwater.org>
2015-03-18 16:10:39 +0000

    added autoform modals, publications

--- server/allow.js
+++ server/allow.js
@@ -3,3 +3,33 @@ Emails.allow({
 		return true
 	}
 });
+
+Events.allow({
+	insert: function (userId, doc) {
+		return true
+	},
+	update: function (userId, doc, fields, modifier) {
+		return true
+	},
+	remove: function (userId, doc) {
+		return true
+	}
+});
+
+Posts.allow({
+	insert: function (userId, doc) {
+		return true
+	},
+	remove: function (userId, doc) {
+		return true
+	}
+});
+
+Comments.allow({
+	insert: function (userId, doc) {
+		return true
+	},
+	remove: function (userId, doc) {
+		return true
+	}
+});

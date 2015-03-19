b60b128bfba9f0d802b4b7246981fcf84b48a5ce
Kwame Asiedu <kwame.asiedu@meltwater.org>
2015-03-19 15:55:03 +0000

    Fixed logging paths

--- both/router.js
+++ both/router.js
@@ -74,5 +74,14 @@ Router.map( function () {
 				occasion: Posts.find()
 			}
 		}
-	});
+	}); 	
+});
+
+
+AccountsTemplates.configureRoute('signIn', {
+    name: 'signin',
+    // path: '/',
+    template: 'home',
+    // layoutTemplate: 'myLayout',
+    redirect: '/events',
 });
